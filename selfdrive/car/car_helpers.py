a4f67a44f41078920177184d3d0b626e1e524865
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-12-22 10:35:34 +0900

    Update car_helpers.py

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -24,8 +24,8 @@ def get_startup_event(car_recognized, controller_available):
     event = EventName.startupNoCar
   elif car_recognized and not controller_available:
     event = EventName.startupNoControl
-  elif EON and "letv" not in open("/proc/cmdline").read():
-    event = EventName.startupOneplus
+#  elif EON and "letv" not in open("/proc/cmdline").read():
+#    event = EventName.startupOneplus
   return event
 
 
