b2455a36614eb5e8ebb8992ef3e2ae7be4483af5
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2021-05-28 14:55:53 +0900

    Update interfaces.py

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -13,7 +13,7 @@ from selfdrive.controls.lib.vehicle_model import VehicleModel
 
 GearShifter = car.CarState.GearShifter
 EventName = car.CarEvent.EventName
-MAX_CTRL_SPEED = (V_CRUISE_MAX + 4) * CV.KPH_TO_MS  # 135 + 4 = 86 mph
+MAX_CTRL_SPEED = (V_CRUISE_MAX + 15) * CV.KPH_TO_MS  # 135 + 15 = 93 mph
 
 # generic car and radar interfaces
 
