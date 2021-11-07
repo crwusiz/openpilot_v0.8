7674a57abda9fa122dd7848e8136194d85ec3bee
crwusiz <crwusiz@gmail.com>
2021-11-08 07:14:59 +0900

    script fix

--- scripts/op3t_neos_update.sh
+++ scripts/op3t_neos_update.sh
@@ -3,9 +3,17 @@
 if [ -z "$BASEDIR" ]; then
   BASEDIR="/data/openpilot"
 fi
-
 source "$BASEDIR/launch_env.sh"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+NEOS_PY="$DIR/selfdrive/hardware/eon/neos.py"
+MANIFEST="$DIR/selfdrive/hardware/eon/op3t.json"
+
+echo "Installing op3t NEOS update"
 cp -f "$BASEDIR/installer/updater/update.zip" "/sdcard/update.zip"
 pm disable ai.comma.plus.offroad
 killall _ui
-"$BASEDIR/selfdrive/hardware/eon/updater" "file://$BASEDIR/selfdrive/hardware/eon/op3t.json"
+
+#"$BASEDIR/selfdrive/hardware/eon/updater" "file://$BASEDIR/selfdrive/hardware/eon/op3t.json"
+$NEOS_PY --swap-if-ready $MANIFEST
+$DIR/selfdrive/hardware/eon/updater $NEOS_PY $MANIFEST
